b3ff7345bbb04e9270ec1888898ad4061efc4396
Kyle <k7for9.eckrich@gmail.com>
2023-09-02 15:24:24 -0500

    added UI

---
+++ Assets/Scenes/tset 1/DraggableItem.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    [HideInInspector]
+    public Transform parentAfterDrag;
+    public Image image;
+    public TMPro.TextMeshProUGUI text;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+
+    // [ContextMenu("Set Text")]
+    // public void SetText()
+    // {
+    //     SetText("test");
+    // }
+    public void SetText(String t)
+    {
+        text.text = t;
+    }
+}
