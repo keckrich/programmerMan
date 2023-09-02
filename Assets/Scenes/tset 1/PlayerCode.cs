db6b12073b90235296a93d889bb690c50f86df9d
Kyle <k7for9.eckrich@gmail.com>
2023-09-02 18:46:34 -0500

    got the move to work in steps

--- Assets/Scenes/tset 1/PlayerCode.cs
+++ Assets/Scenes/tset 1/PlayerCode.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCode : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject parentObject;  // Assign this from the editor or from another script
+    private DraggableItem grandchild;
     void Start()
     {
 
@@ -20,5 +22,49 @@ public class PlayerCode : MonoBehaviour
     public void OnStartButton()
     {
         Debug.Log("clocked");
+        List<DraggableItem> grandchildScripts = GetDragableItems();
+        StartCoroutine(CallFunctionOnItems(grandchildScripts));
+    }
+
+    private IEnumerator CallFunctionOnItems(List<DraggableItem> grandchildScripts)
+    {
+        foreach (DraggableItem gc in grandchildScripts)
+        {
+            ProcessItem(gc);
+            yield return new WaitForSeconds(2); // Wait for 2 seconds
+        }
+    }
+
+    private void ProcessItem(DraggableItem item)
+    {
+        item.onExecute();
+    }
+
+    public List<DraggableItem> GetDragableItems()
+    {
+        // List to hold all the script instances found on the grandchildren
+        List<DraggableItem> grandchildScripts = new();
+
+        // Traverse all children
+        foreach (Transform child in parentObject.transform)
+        {
+            Debug.Log("child");
+            // Traverse all grandchildren of each child
+            foreach (Transform grandchild in child)
+            {
+                Debug.Log("grandchild");
+
+                // Try to get the script component on the grandchild
+                DraggableItem script = grandchild.GetComponent<DraggableItem>();
+
+                // If the script component exists, add it to the list
+                if (script != null)
+                {
+                    grandchildScripts.Add(script);
+                }
+            }
+        }
+
+        return grandchildScripts;
     }
 }
